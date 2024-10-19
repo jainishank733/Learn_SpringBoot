750da4100e906478402e5e0de4e309d28d1b195a
Your Name <you@example.com>
2024-10-19 20:11:57 +0530

    first commit

---
+++ src/test/java/com/in28minutes/springboot/learn_spring_boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.learn_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.in28minutes.springboot.learnspringboot.LearnSpringBootApplication;
+
+@SpringBootTest(classes = LearnSpringBootApplication.class)
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
